8fadcbd467a3ef2b686402091320c1721022f1f6
cryjay2017 <nian.cai@live.com>
2019-04-07 21:14:31 +0800

    Create post files to application

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,74 +1,46 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-
-  # GET /posts
-  # GET /posts.json
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
-
-  # GET /posts/1
-  # GET /posts/1.json
-  def show
-  end
-
-  # GET /posts/new
+  
   def new
-    @post = Post.new
+    @post = Post.new 
   end
-
-  # GET /posts/1/edit
-  def edit
-  end
-
-  # POST /posts
-  # POST /posts.json
+  
   def create
     @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
     end
   end
-
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
+  
+  def show
+  end
+  
+  def edit
+  end
+  
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
     end
   end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
+  
   def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to root_path if @post.destroy
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:title, :content, :author)
-    end
+ private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+  
+  def find_post
+    @post = Post.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_16_031739) do
+ActiveRecord::Schema.define(version: 2019_03_31_114023) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
